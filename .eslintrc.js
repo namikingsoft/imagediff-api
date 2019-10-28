87cbc913f6743fa19c0228a35d488ecba6220a97
Tsubasa Namiki <namikingsoft@gmail.com>
2019-10-29 07:54:44 +0900

    test: move spec file to src

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
   extends: [
     'airbnb-base',
     'plugin:flowtype/recommended',
@@ -11,6 +16,7 @@ module.exports = {
     'import/prefer-default-export': 0,
     'no-mixed-operators': 0,
     'no-continue': 0,
+    'no-console': 0,
     'arrow-parens': [2, 'as-needed'],
   },
 };
--- src/lib.spec.js
+++ src/lib.spec.js
@@ -1,7 +1,8 @@
 import {
   diffImage,
-} from 'lib';
+} from './lib';
 
+// eslint-disable-next-line no-undef
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
 describe('lib', () => {
