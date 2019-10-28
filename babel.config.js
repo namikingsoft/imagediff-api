c68d279daee6ccfcf7dbd1702f0326ac9fc9a8ca
Tsubasa Namiki <namikingsoft@gmail.com>
2019-10-29 08:08:53 +0900

    chore: update babel to latest

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-flow',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
+  ],
+};
